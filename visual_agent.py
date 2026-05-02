e77b85c4dfe9b8474a5286b82971025d7bcd9833
kfcwriters-cloud <kfcwriters@gmail.com>
2026-05-02 22:01:37 +0530

    Update visual_agent.py

--- visual_agent.py
+++ visual_agent.py
@@ -2,14 +2,12 @@ from manim import *
 
 class VisualShort(Scene):
     def construct(self):
-        # Romantic Aesthetic
         bg = FullScreenRectangle(fill_opacity=1).set_color("#1A0F0F")
         self.add(bg)
         
-        # Simple Elegant Frame
         frame = SurroundingRectangle(FullScreenRectangle(), color="#D4AF37", buff=-0.5)
         title = Text("VELVET HOURS", font="Sans", size=0.6, color="#D4AF37").to_edge(UP)
         
         self.play(FadeIn(bg), Create(frame), Write(title), run_time=3)
         self.play(Indicate(title), run_time=5)
-        self.wait(17) # Total 25 seconds
+        self.wait(17)
